f7cbf7b43f4184ea81ad36dcb42ca6f4cc20a662
sparkzy <31968082+sparkzy@users.noreply.github.com>
2020-11-24 21:16:25 -0500

    first commit

---
+++ src/test/java/gg/sparkzy/casl/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package gg.sparkzy.casl.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
